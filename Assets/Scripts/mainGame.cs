8144e7dd8587e031242daa414ab614be0c977b09
Marco Alexander Iza Quinatoa <marcoizaq@gmail.com>
2022-01-16 10:55:01 -0500

    Lifes

--- Assets/Scripts/mainGame.cs
+++ Assets/Scripts/mainGame.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class mainGame : MonoBehaviour
 {
+    public GameObject[] hearts;
+    private int life;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        life = hearts.Length;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (life < 1) {
+            Destroy(hearts[0].gameObject);
+        }
+        else if (life < 2)
+        {
+            Destroy(hearts[1].gameObject);
+        }
+        else if (life < 3)
+        {
+            Destroy(hearts[2].gameObject);
+        }
     }
+
+
 }
